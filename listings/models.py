0d9cc1ce5280f5e797a4bbffc61ca14a5b2b31d4
mhamzabaig <mhamzabaig1234@gmail.com>
2023-05-14 16:46:42 +0500

    Listing model added

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    no_of_bedrooms = models.IntegerField()
+    no_of_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=100)
+    # image = models.ImageField()
+
